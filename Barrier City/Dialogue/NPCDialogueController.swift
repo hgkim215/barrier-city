0c1a6b910547063e30cf460a7772bd811eb5c2b7
Hyeongi Kim (Wade) <21900215@handong.ac.kr>
2026-08-21 02:07:07 +0900

    fix: align realtime mission coordinator initialization in dialogue controller

--- Barrier City/Dialogue/NPCDialogueController.swift
+++ Barrier City/Dialogue/NPCDialogueController.swift
@@ -113,7 +113,7 @@ final class NPCDialogueController {
         self.orderSession = orderSession
         self.accessibilityAttitude = accessibilityAttitude
         self.clerkPersonality = resolvedPersonality
-        realtimeMission = RealtimeMissionCoordinator(personality: resolvedPersonality)
+        realtimeMission = RealtimeMissionCoordinator()
         climate = initialClimate
         rapport = initialClimate.rapport
         tone = initialClimate.tone
