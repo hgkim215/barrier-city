108fe8c398ffbf8179783a711e9229f05000a633
Noah-1004 <shk001004@gmail.com>
2026-08-26 15:58:54 +0900

    [fixed] Remove Splash volume baseplate
    스플레기 화면 볼륨 윈도우 베이스플레이트 제거

--- Barrier City/Barrier_CityApp.swift
+++ Barrier City/Barrier_CityApp.swift
@@ -33,6 +33,10 @@ struct Barrier_CityApp: App {
 
         WindowGroup(id: AppSceneID.splash) {
             SplashOverlayView()
+                // 볼륨은 사용자가 '바닥' 쪽을 볼 때와 리사이즈 중에 반투명
+                // 베이스플레이트를 띄운다. 스플래시는 이미지 한 장만 보여주므로
+                // 그 판이 그대로 노출돼 거슬린다. (Scene이 아니라 View 모디파이어)
+                .volumeBaseplateVisibility(.hidden)
         }
         .windowStyle(.volumetric)
         .defaultSize(width: 0.6, height: 0.6, depth: 0.01, in: .meters)
